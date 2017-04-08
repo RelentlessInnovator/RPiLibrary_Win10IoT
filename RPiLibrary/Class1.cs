69c81cbe0239519f62b585a3075ec22c1fdc8ccc
Jordan Moss <kk4jrq@gmail.com>
2017-04-08 17:45:34 -0400

    Add project files.

---
+++ RPiLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPiLibrary
+{
+    public class Class1
+    {
+    }
+}
